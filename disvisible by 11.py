84a0612c8036e5a6d55b6cecabf19d91a0f617cd
Badru Khamisi <146442651+badrutendo@users.noreply.github.com>
2025-01-30 14:12:08 +0300

    Update disvisible by 11.py

--- disvisible by 11.py
+++ disvisible by 11.py
@@ -1,4 +1,5 @@
-number=int(input("Enter a number:"))
+number=int(input("Enter a number:"))#converts input to an integer
+#check if the number is divisible by 11 
 if (number%11 ==0):
     print(str(number) +" is divisible by 11")
 else:
