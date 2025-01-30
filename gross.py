1baafef456ad49bb9bcb6513aa63b5d125fe064f
Badru Khamisi <146442651+badrutendo@users.noreply.github.com>
2025-01-30 14:17:39 +0300

    Update gross.py

--- gross.py
+++ gross.py
@@ -1,4 +1,7 @@
+#Get users input for hours worked and rate
 hours = float(input("Enter hours:"))
 rate_per_hour=float(input("Enter:"))
+#calculate the gross pay
 gross_pay= hours*rate_per_hour
+#Print gross pay 
 print("Gross pay is", gross_pay)
