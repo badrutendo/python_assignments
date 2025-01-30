19ef4d9f4e319ce9c30c6ac4c52298cd788cfc19
Badru Khamisi <146442651+badrutendo@users.noreply.github.com>
2025-01-30 14:20:34 +0300

    Update voting.py

--- voting.py
+++ voting.py
@@ -1,5 +1,7 @@
+#Prompt user to enter their citizenship and nationality
 citizenship = input("Enter Citizenship:")
 age = int(input("Enter Age:"))
+#check if the user qualifies for voting 
 if (citizenship == "Kenyan" or citizenship == "Ugandan" or citizenship == "Tanzanian"and age>=18):
     print("You are eligible to vote!")
 else:
